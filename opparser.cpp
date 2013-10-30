cfac0810b76fd1955fa703b1dbedbdcda9998902
hcz <h.cz@qq.com>
2013-10-30 15:48:27 +0800

    init

---
+++ opparser.cpp
@@ -0,0 +1,96 @@
+#include "opparser.hpp"
+
+namespace OPParser {
+    void error(const string info) {
+        throw runtime_error(info);
+    }
+
+    void check(const bool condition, const string info) {
+        if (!condition) {
+            error(info);
+        }
+    }
+
+    // An abstract token to pop everything from middle stack
+    class FinToken: public Token {
+    public:
+        Level levelLeft() {
+            return numeric_limits <Level>::min();
+        }
+        Level levelRight() {
+            error("Input already finished");
+            // Never reach
+            return numeric_limits <Level>::max();
+        }
+        void onPush(Parser &parser) {
+            //
+        }
+        void onPop(Parser &parser) {
+            //
+        }
+    };
+
+    void Parser::midPush(PToken &token) {
+        token->onPush(*this);
+
+        if (!midStack.empty()) {
+            while (1) {
+                // Pop all lower-level tokens
+                if (midStack.back()->levelRight() > token->levelLeft()) {
+                    midPop();
+                    continue;
+                }
+                if (midStack.back()->levelRight() < token->levelLeft()) {
+                    break;
+                }
+                // Wrong
+                error("Token collision");
+            }
+        }
+
+        midStack.push_back(token);
+    }
+
+    // Pop from middle stack
+    void Parser::midPop() {
+        midStack.back()->onPop(*this);
+        // outstack.push_back(midStack.back());
+        midStack.pop_back();
+    }
+
+    void Parser::parse(const Input &input) {
+        InputIter now = input.begin();
+        const InputIter end = input.end();
+        while (now != end) {
+            vector <PLexer> &nowlexers = lexers[state];
+
+            // Scan the lexer chain
+            vector <PLexer>::iterator iter = nowlexers.begin();
+            while (1) {
+                check(iter != nowlexers.end(), "Unknown token");
+
+                if ((*iter)->tryGetToken(now, end, *this)) {
+                    break;
+                }
+
+                ++iter;
+            }
+        }
+    }
+
+    void Parser::finish(vector <PToken> &result) {
+        // Clear middle stack
+        FinToken _finTokenInstance;
+        PToken finToken = &_finTokenInstance;
+        midPush(finToken);
+
+        check(midStack.size() == 1, "Input not completed");
+        // assert(midStack[0] == finToken);
+
+        midStack.clear();
+
+        // Return outstack as result
+        result = outStack;
+        outStack.clear();
+    }
+}
---
+++ opparser.hpp
@@ -0,0 +1,86 @@
+#ifndef __INC_OPPARSER_HPP__
+#define __INC_OPPARSER_HPP__
+
+#include <stdexcept>
+#include <vector>
+#include <map>
+#include <string>
+#include <limits>
+
+using namespace std;
+
+namespace OPParser {
+    typedef int State;
+    typedef int Level;
+    typedef string Input;
+    typedef Input::const_iterator InputIter;
+
+    class Lexer;
+    class Token;
+    class Parser;
+
+    typedef Lexer *PLexer;
+    typedef Token *PToken;
+
+    // Throw error
+    void error(const string info);
+
+    // Runtime checking like assert
+    // If failed, throw error
+    void check(const bool condition, const string info);
+
+    // Lexer particle, recognise token from string
+    // Chain-factory, to create token
+    class Lexer {
+    public:
+        // If can create this token, return true, increase the offset of input and push token
+        // If input can be ignored, return true and increase the offset
+        // If nothing can be done, return false and throw the task to next lexer
+        virtual bool tryGetToken(InputIter &now, const InputIter &end, Parser &parser) = 0;
+    };
+
+    // Token, contains token information and final data (after pop from middle stack)
+    class Token {
+    public:
+        // Precedence levels
+        virtual Level levelLeft() = 0;
+        virtual Level levelRight() = 0;
+
+        // Push to middle stack
+        // To change state of lexers
+        virtual void onPush(Parser &parser) = 0;
+
+        // Pop from middle stack
+        // To build final data (using data from output stack)
+        // And to push data to output stack
+        virtual void onPop(Parser &parser) = 0;
+    };
+
+    // The operator-precedence parser
+    // Using modified shunting-yard algorithm
+    class Parser {
+    protected:
+        // Map of lexer chains
+        map <State, vector <PLexer> > lexers;
+
+        vector <PToken> midStack;
+        // vector <PToken> outStack;
+    public:
+        State state;
+        vector <PToken> outStack;
+
+        // Push to middle stack
+        void midPush(PToken &token);
+
+        // Pop from middle stack
+        void midPop();
+
+        // Parse a string
+        void parse(const Input &input);
+
+        // Finish parsing
+        void finish(vector <PToken> &result);
+    };
+}
+
+#endif
